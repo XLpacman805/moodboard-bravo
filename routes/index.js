53291ccbab1e33f4780318f3d3e8b53ec96fa46d
Johnny <meza.johnny96@gmail.com>
2018-03-13 21:35:46 -0700

    Changed the input to only take text. It trims the url down so that it gets just the imgur album hash. Will need to write a module that interacts with the API. Perhaps write a promise so that the returning value gets rendered.

--- routes/index.js
+++ routes/index.js
@@ -3,29 +3,12 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  let imgurAlbum = req.query.albumlink.toString().replace('https://imgur.com/a/', ''); //removes the URL part.
+  console.log(imgurAlbum);
 
-    let imageOne = req.query.imageone;
-    let imageTwo = req.query.imagetwo;
-    let imageThree = req.query.imagethree;
-    let imageFour = req.query.imagefour;
-    let imageFive = req.query.imagefive;
-    let imageSix = req.query.imagesix;
-    let imageSeven = req.query.imageseven;
-    let imageEight = req.query.imageeight;
-
-    console.log(imageOne);
 
   res.render('index', {
-    title: 'Moodboard Bravo',
-    imageOne: imageOne,
-    imageTwo: imageTwo,
-    imageThree: imageThree,
-    imageFour: imageFour,
-    imageFive: imageFive,
-    imageSix: imageSix,
-    imageSeven: imageSeven,
-    imageEight: imageEight
-
+    title: 'Moodboard Bravo'
   });
 });
 
