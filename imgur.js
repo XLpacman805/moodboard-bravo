1c15807c319c7338a009e33bb0656304059aa79b
Johnny <meza.johnny96@gmail.com>
2018-03-14 18:30:45 -0700

    Added an Imgur module to handle the interaction with imgur api. Wrote a promise for imgurAlbum.

---
+++ imgur.js
@@ -0,0 +1,1 @@
+exports.test = (x) => x + " HELLO WORLD";
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const imgur = require('../imgur.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let imgurAlbum = req.query.albumlink.toString().replace('https://imgur.com/a/', ''); //removes the URL part.
-  console.log(imgurAlbum);
+
+    let imgurAlbum = new Promise((resolve, reject) => { // I promise i'll get you a link
+      resolve(req.query.albumlink.toString().replace('https://imgur.com/a/', ''));
+      reject("Something went wrong");
+    });
+
+    imgurAlbum.then((albumHash) => { // then when I get the link, do this.
+      console.log(imgur.test(albumHash));
+    })
+        .catch(reason => console.log(reason)); // If for some reason you catch an error, log the reason.
 
 
   res.render('index', {
