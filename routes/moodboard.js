b424f80344d7c3370f353e7af3c36f443ff46aaf
Johnny <meza.johnny96@gmail.com>
2018-03-15 18:16:30 -0700

    Created a new view called moodboard. When a user enters an imgur link it'll take them to mysitle/moodboard/albumHash. The album hash will be used with the imgur api.

---
+++ routes/moodboard.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const imgur = require('../imgur.js');
+
+/* GET moodboard page. */
+
+router.get('/', (req, res, next) => {
+    res.end('HELLO WORLD');
+
+});
+
+module.exports = router;
