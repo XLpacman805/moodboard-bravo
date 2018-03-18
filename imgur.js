fcbb8bc906e9943aaf4bf0b12ec7d298788fe3ca
Johnny <meza.johnny96@gmail.com>
2018-03-18 13:48:54 -0700

    Need to figure out how to make https requests in nodejs.

--- imgur.js
+++ imgur.js
@@ -6,6 +6,6 @@ exports.getImagesFromAlbum = (albumHash) => {
     return request('https://api.imgur.com/3/album/' + albumHash +'/images/?client_id=' + process.env.IMGUR_CLIENT_ID, (err, res, body) =>{
         if (err) throw err;
 
-        return JSON.parse(body);
+        return JSON.parse(body); // wow
     });
 };
