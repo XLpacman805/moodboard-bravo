d3efbf73a988bcbb8e95f59b7d217ceeb1cd64b0
Johnny <meza.johnny96@gmail.com>
2018-03-17 20:00:57 -0700

    changes.

---
+++ public/javascripts/index.js
@@ -0,0 +1,5 @@
+console.log("HELLO FRONT END WORLD");
+
+document.getElementById('submit-button').addEventListener('click', () => {
+
+});
--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const imgur = require('../imgur.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-    let imgurAlbum = new Promise((resolve, reject) => { // I promise i'll get you a link
-      resolve(req.query.albumlink.toString().replace('https://imgur.com/a/', ''));
-      reject("Something went wrong");
-    });
-
-    imgurAlbum
-        .then((albumHash) => {
-            return albumHash;
-        })
-        .catch(reason => console.log(reason)); // If for some reason you catch an error, log the reason.
-
-
-    res.render('index', {title: 'Moodboard Bravo', test: imgurAlbum});
+    res.render('index', {title: 'Moodboard Bravo'});
 });
-
 
 module.exports = router;
