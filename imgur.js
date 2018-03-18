3ba5f18d8d6f1f5a66ae3cfefdbb3329f21ddde4
Johnny <meza.johnny96@gmail.com>
2018-03-18 13:48:06 -0700

    Deleted the index.js public js file. I just realised I don't need to do any complicated DOM manipulation when i can just redired to another page.

--- imgur.js
+++ imgur.js
@@ -1,1 +1,11 @@
+const request = require('request');
+
 exports.test = (x) => x + " HELLO WORLD";
+
+exports.getImagesFromAlbum = (albumHash) => {
+    return request('https://api.imgur.com/3/album/' + albumHash +'/images/?client_id=' + process.env.IMGUR_CLIENT_ID, (err, res, body) =>{
+        if (err) throw err;
+
+        return JSON.parse(body);
+    });
+};
--- routes/moodboard.js
+++ routes/moodboard.js
@@ -5,8 +5,12 @@ const imgur = require('../imgur.js');
 /* GET moodboard page. */
 
 router.get('/', (req, res, next) => {
+    // Parses the imgur album URL so that we just get the ending hash.
     let albumHash = req.query.albumlink.toString().replace("https://imgur.com/a/", "");
-    res.end(albumHash);
+
+    console.log(imgur.getImagesFromAlbum(albumHash));
+
+    res.end("dosdksdfk");
 
 });
 
