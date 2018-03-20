5a53fd5d782543befd9f860a8cd4d31e036d136e
Johnny <meza.johnny96@gmail.com>
2018-03-19 21:13:56 -0700

    Can grab all images from an imgur album and render them in a flexbox. Next is to grab titles and descriptions from imgur albumm.

--- imgur.js
+++ imgur.js
@@ -3,8 +3,8 @@ const https = require('https');
 exports.test = (x) => x + " HELLO WORLD";
 
 // Sends GET request to imgur api for our album. A callback must be passed into this function to use the GET response.
-exports.getImagesFromAlbum = (albumHash, cb) => {
-   https.get('https://api.imgur.com/3/album/' + albumHash + '/images/?client_id=' + process.env.IMGUR_CLIENT_ID,
+exports.getAlbum = (albumHash, cb) => {
+   https.get('https://api.imgur.com/3/album/' + albumHash + '/?client_id=' + process.env.IMGUR_CLIENT_ID,
        res =>{
             let body = '';
             res.on('data', data => {
--- routes/moodboard.js
+++ routes/moodboard.js
@@ -8,9 +8,13 @@ router.get('/', (req, res, next) => {
     // Parses the imgur album URL so that we just get the ending hash.
     let albumHash = req.query.albumlink.toString().replace("https://imgur.com/a/", "");
     // Gets an Array of all images from the album.
-    imgur.getImagesFromAlbum(albumHash, images => {
-        console.log(images);
-        res.render('moodboard', {images: images});
+    imgur.getAlbum(albumHash, album => {
+        res.render('moodboard', {
+            albumTitle: album.title,
+            albumDescription: album.description,
+            images: album.images
+
+        });
     });
 });
 
